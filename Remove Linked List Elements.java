41091d0a65cd25fc9cec4797ee4424e88899fd95
Aditi Gupta <60029443+Aditi200@users.noreply.github.com>
2020-07-21 01:13:05 +0530

    Add files via upload

---
+++ Remove Linked List Elements.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+  
+        public ListNode removeElements(ListNode head, int val) {
+    ListNode helper = new ListNode(0);
+    helper.next = head;
+    ListNode p = helper;
+ 
+    while(p.next != null){
+        if(p.next.val == val){
+            ListNode next = p.next;
+            p.next = next.next; 
+        }else{
+            p = p.next;
+        }
+    }
+ 
+    return helper.next;
+}
+    }
